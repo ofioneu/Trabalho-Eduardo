757a4cac45fa5af3c2075a5a91e024c6a26b9958
Fabio Martins <fmfabio.0991@yahoo.com.br>
2019-11-29 18:06:57 -0300

    Corrigido e comentado as linhas

--- du.py
+++ du.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask  #Flask é um pequeno framework web escrito em Python e baseado na biblioteca WSGI Werkzeug e na biblioteca de Jinja2. 
 from flask import render_template
 import json
 from flask_socketio import SocketIO
@@ -17,44 +17,48 @@ try:
 
 
 
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
-    print("Falha conexao com o banco!")
+except :
+    print ("Erro ao tentar abrir o dataset!")
+    
 
 finally:
  
     @app.route('/')
     def grafic():
-        frame = pd.DataFrame(df)
-        frame.dropna(inplace=True)
-        frame1 = frame[(frame['mensalidade']>3000) & (frame['universidade_nome'])]
-        #rel_mens_uf = frame1[['uf_busca', 'mensalidade']]
-        dados_mens = frame1['mensalidade']
-        dados_uni_name = frame1['universidade_nome']
-        print(dados_uni_name)
-        vet_dados_mens=[]
-        vet_dados_uini_name=[]
-        for i in dados_mens:
+        frame = pd.DataFrame(df) #Cria um dataframe do dataset
+        frame.dropna(inplace=True) #exclui as linhas que contem um Not a Number (NaN)
+        frame1 = frame[(frame['mensalidade']>6000) & (frame['universidade_nome'])].sort_values('mensalidade',ascending=False)  #cria um dataframe relacionando a mensalidade com os nomes das instituições cujo a mensalidade é superior a 6000,00
+        dados_mens = frame1['mensalidade'] # cria uma série da coluna mensalidade embasada na relação frame1
+        dados_uni_name = frame1['universidade_nome'] # cria uma série da coluna universidade_nome embasada na relação frame1
+        vet_dados_mens=[] #lista para os valores da serie dados_mens
+        vet_dados_uini_name=[] #lista para os valores da serie vet_dados_uini_name
+        vet_dados_notas_corte_ampla=[]  #lista para os valores da serie vet_dados_notas_corte_ampla
+
+        for i in dados_mens: #loop para interar a série dados_mens na lista vet_dados_mens
             vet_dados_mens.append(i)
-        for j in dados_uni_name:
-            vet_dados_uini_name.append(j)
-            print(j)
         
+        for j in dados_uni_name: #loop para interar a série dados_mens na lista dados_uni_name
+            vet_dados_uini_name.append(j)
+
+        #Bivlioteca python       
         dados_json = {
             "dados_mens": vet_dados_mens,
             "dados_uni_name": vet_dados_uini_name
+            
             }
-
+        
+        #converte a biblioteca dados_json em objeto json
         values_json = json.dumps(dados_json)
         
-
+        #cria uma conexão com o socket
         @socketio.on('message')
         def menssagem(message):
             print(message) 
-            emit('message', values_json)
-            #time.sleep(1)
-        return render_template('home.html')
+            emit('message', values_json) #envia o objeto values_json para o cliente
+            
+        return render_template('home.html') #renderiza o html
 
+#configura o server
 if __name__ == '__main__':
     socketio.run(app,host='0.0.0.0', port= 8888)
    
